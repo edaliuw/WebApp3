d4bf8eb2fc2003b642b2750a308b5418ce1b764f
edaliuw <36862429+edaliuw@users.noreply.github.com>
2019-08-18 00:10:25 -0700

    Minor changes to fit requirements

---
+++ Controllers/RoundsController.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using tourneybracket.Data;
+using tourneybracket.Models;
+
+namespace tourneybracket.Controllers
+{
+    public class RoundsController : Controller
+    {
+        private readonly BracketContext _context;
+
+        public RoundsController(BracketContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Rounds
+        public async Task<IActionResult> Index()
+        {
+            var bracketContext = _context.Rounds.Include(r => r.Bracket);
+            return View(await bracketContext.ToListAsync());
+        }
+
+        // GET: Rounds/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var round = await _context.Rounds
+                .Include(r => r.Bracket)
+                .FirstOrDefaultAsync(m => m.id == id);
+            if (round == null)
+            {
+                return NotFound();
+            }
+
+            return View(round);
+        }
+
+        // GET: Rounds/Create
+        public IActionResult Create()
+        {
+            ViewData["BracketID"] = new SelectList(_context.Brackets, "id", "BracketName");
+            return View();
+        }
+
+        // POST: Rounds/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("id,BracketID,RoundNumber")] Round round)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(round);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["BracketID"] = new SelectList(_context.Brackets, "id", "BracketName", round.BracketID);
+            return View(round);
+        }
+
+        // GET: Rounds/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var round = await _context.Rounds.FindAsync(id);
+            if (round == null)
+            {
+                return NotFound();
+            }
+            ViewData["BracketID"] = new SelectList(_context.Brackets, "id", "BracketName", round.BracketID);
+            return View(round);
+        }
+
+        // POST: Rounds/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("id,BracketID,RoundNumber")] Round round)
+        {
+            if (id != round.id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(round);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!RoundExists(round.id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["BracketID"] = new SelectList(_context.Brackets, "id", "BracketName", round.BracketID);
+            return View(round);
+        }
+
+        // GET: Rounds/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var round = await _context.Rounds
+                .Include(r => r.Bracket)
+                .FirstOrDefaultAsync(m => m.id == id);
+            if (round == null)
+            {
+                return NotFound();
+            }
+
+            return View(round);
+        }
+
+        // POST: Rounds/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var round = await _context.Rounds.FindAsync(id);
+            _context.Rounds.Remove(round);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool RoundExists(int id)
+        {
+            return _context.Rounds.Any(e => e.id == id);
+        }
+    }
+}
